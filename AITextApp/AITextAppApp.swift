5d8cc2bd65eab5ac02fee5a6a8efdc5453c1be62
Eva Chlpikova <chlpikova.e@gmail.com>
2024-02-18 13:52:02 +0100

    Initial Commit

---
+++ AITextApp/AITextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITextAppApp.swift
+//  AITextApp
+//
+//  Created by Eva Chlpikova on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AITextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
